9e0b9e2b537f8f9938cafae1952e93fdcc16e83d
DeimosC1 <62855417+DeimosC1@users.noreply.github.com>
2021-02-04 17:32:16 +0100

    Update daZoo.java

--- src/main/java/cz/educanet/webik/daZoo.java
+++ src/main/java/cz/educanet/webik/daZoo.java
@@ -1,5 +1,7 @@
 package cz.educanet.webik;
+
 import java.util.Scanner;
+import java.util.ArrayList;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -7,20 +9,44 @@ import javax.ws.rs.core.Response;
 @Path("Zoo") // <-- localhost:8080/[nazevAppky]/api/Zoo
 @Produces(MediaType.APPLICATION_JSON) // <--  nastavuji jak chci komunikovat
 public class daZoo {
-   public String[] animols;
 
+    private static final ArrayList<String> Animels = new ArrayList<String>();
 
 
     @GET
-    public Response getAllPayments() {
-        return Response.ok(String.valueOf(animols)).build();
+    public Response getAnimol(@QueryParam("getAnimol") int id) {
+        if (id <= Animels.size()) {
+            if (id == 0) {
+                return Response.ok(Animels).build();
+            } else {
+                return Response.ok(Animels.get(id)).build();
+            }
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
     }
-
     @POST
-    public Response decrement() {
-    Scanner newAnimol = new Scanner(System.in);
-    animols = new String[0]; //nedávám
-
+    public Response spawnAnimal(@QueryParam("newAnimal") String newAnimal) {
+        if (newAnimal == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        Animels.add(newAnimal);
         return Response.ok().build();
     }
+
+    @PUT
+    public Response putAnimol(@QueryParam("id") String newAnimlo) {
+        return Response.ok().build();
+
+    }
+    @DELETE
+    public Response killAniml(@QueryParam("getAnimal") int id) {
+        if (id < 0) {
+            return Response.ok(Animels).build();
+        }
+        Animels.remove(id);
+        return Response.ok("kealed " + Animels.get(id)).build();
+    }
 }
+
